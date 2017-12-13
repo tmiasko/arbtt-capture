cbd5c7f6ac6e06fe0561a7179935a8163942ccb0
Tomasz Miąsko <tomasz.miasko@gmail.com>
2017-12-13 00:00:00 +0000

    Append entries to activity log.

--- extension.js
+++ extension.js
@@ -1,68 +1,179 @@
 var {
     init, 
-    enable, 
-    disable
 } = (function() {
     'use strict';
 
+    const ByteArray = imports.byteArray;
     const GLib = imports.gi.GLib;
+    const Gio = imports.gi.Gio;
     const Main = imports.ui.main;
     const Meta = imports.gi.Meta;
     
-    const Me = imports.misc.extensionUtils.getCurrentExtension();
-    const Convenience = Me.convenience;
+    const Extension = imports.misc.extensionUtils.getCurrentExtension();
+    const Settings = Extension.imports.settings;
 
-    function logWindows() {
-        log("###########");
-        const windows = global.get_window_actors().map((w) => w.get_meta_window());
-        for (const window of windows) {
-            log(`window title: ${window.get_title()}`);
-            log(`program name: ${window.get_wm_class_instance()}`);
+    class Capture {
+
+        constructor() {
+            this._timeoutId = null;
+            this._settings = Settings.getSettings();
         }
 
+        enable() {
+            const interval = this._settings.get_uint('sampling-interval');
+            const file = Gio.File.new_for_path(this._settings.get_string('log-path'));
+            this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_LOW, interval, () => {
+                const entry = generateLogEntry();
+                entry.interval = interval;
+                const entryBytes = serialize(entry);
+
+                const insertHeader = !file.query_exists(null);
+                const stream = Gio.DataOutputStream.new(file.append_to(Gio.FileCreateFlags.NONE, null));
+                if (insertHeader) {
+                    stream.write_all(MAGIC, null);
+                }
+                stream.write_all(entryBytes.get_data(), null);
+                stream.close(null);
+                return true;
+            });
+        }
+
+        disable() {
+            if (this._timeoutId !== null) {
+                GLib.source_remove(this._timeoutId);
+                this._timeoutId = null;
+            }
+        }
+    };
+
+    function generateLogEntry() {
+        const focused = global.display.get_focus_window();
+        const windows = global.get_window_actors().map((window) => {
+            const meta = window.get_meta_window();
+            return {
+                title: meta.get_title(),
+                program: meta.get_wm_class_instance(),
+                active: meta == focused,
+            }
+        });
+
         const workspaceIndex = global.screen.get_active_workspace_index();
-        log(`workspace:    ${Meta.prefs_get_workspace_name(workspaceIndex)}`);
+        const workspace = Meta.prefs_get_workspace_name(workspaceIndex);
 
         const currentTime = global.display.get_current_time_roundtrip();
         const userTime = global.display.get_last_user_time();
-        const idleDuration = currentTime - userTime;
-        log(`idle: ${idleDuration / 1000}s`);
+        const idle = currentTime - userTime;
 
-        const focused = global.display.get_focus_window();
-        if (focused == null) {
-            log("WARNING: no focus");
-        } else {
-            log(focused);
-            log("CONTAINS: " + windows.includes(focused));
+        return {
+            timestamp: new Date(),
+            desktop: workspace,
+            idle: idle,
+            windows: windows,
         }
     }
 
     function init() {
-        log('initialized');
-
-        let settings = convenience.getSettings();
+        return new Capture();
     }
 
-    let timeoutId = null;
-    function enable() {
-        timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_LOW, 5, () => {
-            logWindows();
-            return true;
-        });
-        log('enabled');
+    const MAGIC = "arbtt-timelog-v1\n";
+
+    function isLeapYear(year) {
+        return (year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0));
     }
 
-    function disable() {
-        if (timeoutId != null) {
-            GLib.source_remove(timeoutId);
-            timeoutId = null;
+    // Converts UTC month and day into day of the year.
+    function dayOfTheYear(date) {
+        const y = date.getUTCFullYear();
+        const m = date.getUTCMonth();
+        const d = date.getUTCDate();
+        const k = m <= 1 ? 0 : (isLeapYear(y) ? -1 : -2);
+        return Math.floor((367 * m + 5) / 12) + k + d;
+    }
+
+    // Converts UTC date into Modified Julian Day (number of days since 1858-11-17).
+    function toModifiedJulianDay(date) {
+        const y = date.getUTCFullYear() - 1;
+        return dayOfTheYear(date) + 365 * y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) - 678576; 
+    }
+
+    // Converts UTC time into total number of milliseconds since midnight.
+    function toMilliseconds(date) {
+        const hours = date.getUTCHours();
+        const minutes = date.getUTCMinutes();
+        const seconds = date.getUTCSeconds();
+        const milliseconds = date.getUTCMilliseconds();
+        return ((hours * 60 + minutes) * 60 + seconds) * 1000 + milliseconds;
+    }
+
+    let file = Gio.File.new_for_path('/home/tm/a.log');
+    const stream = Gio.DataOutputStream.new(file.append_to(Gio.FileCreateFlags.NONE, null));
+
+    function serialize({timestamp, interval, desktop, idle, windows}) {
+        const memoryStream = Gio.MemoryOutputStream.new_resizable();
+        const stream = Gio.DataOutputStream.new(memoryStream);
+
+        // TimeLogEntry version tag.
+        stream.put_byte(1, null);
+        writeTimestamp(stream, timestamp);
+        writeInteger(stream, interval);
+        // Data version tag.
+        stream.put_byte(3, null);
+        stream.put_int64(windows.length, null);
+        for (const {active, title, program} of windows) {
+            writeBool(stream, active);
+            writeString(stream, title);
+            writeString(stream, program);
         }
-        log('disabled');
+        writeInteger(stream, idle);
+        writeString(stream, desktop);
+
+        stream.close(null);
+        return memoryStream.steal_as_bytes();
+    }
+
+    function writeBool(stream, value) {
+        stream.put_byte(value ? 1 : 0, null);
+    }
+
+    function writeInt64(stream, value) {
+        stream.put_int64(value, null);
+    }
+
+    const INT32_MIN = -(2**31);
+    const INT32_MAX = 2**31-1;
+
+    function writeInteger(stream, value) {
+        if (INT32_MIN <= value && value <= INT32_MAX) {
+            stream.put_byte(0, null);
+            stream.put_int32(value, null);
+        } else {
+            throw new Error('not implemented yet');
+        }
+    }
+
+    function writeString(stream, string) {
+        stream.put_byte(0, null);
+
+        let length = 0;
+        for (const codePoint of string) { 
+            length += 1;
+        }
+
+        stream.put_int64(length, null);
+        const bytes = ByteArray.fromString(string, "UTF-8");
+        stream.write_all(bytes, null);
+    }
+
+    function writeTimestamp(stream, date) {
+        const modifiedJulianDay = toModifiedJulianDay(date);
+        const totalMs = toMilliseconds(date);
+        writeInteger(stream, modifiedJulianDay);
+        writeInteger(stream, totalMs);
+        writeInteger(stream, 1000);
     }
 
     return {
         init: init,
-        enable: enable,
-        disable: disable
     }
 }());
